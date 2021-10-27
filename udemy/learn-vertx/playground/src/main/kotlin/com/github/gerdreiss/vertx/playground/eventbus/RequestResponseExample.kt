42135cf203c19c377c36a459af6e22848ee7bc37
Gerd Reiss <gerd@reiss.pro>
2021-10-27 14:45:42 +0200

    Event Bus - Custom Message Coded: JsonObject

--- udemy/learn-vertx/playground/src/main/kotlin/com/github/gerdreiss/vertx/playground/eventbus/RequestResponseExample.kt
+++ udemy/learn-vertx/playground/src/main/kotlin/com/github/gerdreiss/vertx/playground/eventbus/RequestResponseExample.kt
@@ -4,6 +4,8 @@ import io.vertx.core.AbstractVerticle
 import io.vertx.core.Promise
 import io.vertx.core.Vertx
 import io.vertx.core.eventbus.DeliveryOptions
+import io.vertx.core.json.JsonArray
+import io.vertx.core.json.JsonObject
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
@@ -16,9 +18,11 @@ class RequestVerticle : AbstractVerticle() {
 
   override fun start(startPromise: Promise<Void>?) {
     startPromise?.complete()
-    val message = "Hello World!"
+    val message = JsonObject()
+      .put("message", "Hello World!")
+      .put("version", 1)
     LOG.debug("Sending $message")
-    vertx.eventBus().request<String>(ADDRESS, message) { result ->
+    vertx.eventBus().request<JsonObject>(ADDRESS, message) { result ->
       LOG.debug("Response: ${result.result().body()}")
     }
   }
@@ -32,10 +36,22 @@ class ResponseVerticle : AbstractVerticle() {
 
   override fun start(startPromise: Promise<Void>?) {
     startPromise?.complete()
-    vertx.eventBus().consumer<String>(RequestVerticle.ADDRESS) { message ->
-      LOG.debug("Received ${message.body()}")
-      message.reply("Received your message", DeliveryOptions().setSendTimeout(10000))
-    }
+    vertx
+      .eventBus()
+      .consumer<JsonObject>(RequestVerticle.ADDRESS) { message ->
+        LOG.debug("Received ${message.body()}")
+
+        val response = message
+          .body()
+          .encode()
+          .split("\\W+".toRegex())
+          .filterNot { it.isBlank() }
+          .fold(JsonArray()) { acc: JsonArray, word: String ->
+            acc.add(word)
+          }
+
+        message.reply(response, DeliveryOptions().setSendTimeout(10000))
+      }
   }
 }
 
