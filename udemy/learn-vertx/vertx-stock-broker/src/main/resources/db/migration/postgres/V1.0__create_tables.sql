75fa7238ad519a64d68d7052d14ddb3612a7bda8
Gerd Reiss <gerd@reiss.pro>
2021-11-02 14:03:20 +0100

    database migration

--- udemy/learn-vertx/vertx-stock-broker/src/main/resources/db/migration/postgres/V1.0__create_tables.sql
+++ udemy/learn-vertx/vertx-stock-broker/src/main/resources/db/migration/postgres/V1.0__create_tables.sql
@@ -3,8 +3,47 @@ BEGIN;
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE IF NOT EXISTS assets (
-  id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-  symbol text NOT NULL
+  symbol VARCHAR PRIMARY KEY
 );
+
+CREATE TABLE IF NOT EXISTS quotes (
+  bid NUMERIC,
+  ask NUMERIC,
+  last_price NUMERIC,
+  volume NUMERIC,
+  asset VARCHAR,
+  FOREIGN KEY (asset) REFERENCES assets(symbol),
+  CONSTRAINT last_price_is_positive CHECK (last_price > 0),
+  CONSTRAINT volume_is_positive CHECK (volume > 0)
+);
+
+INSERT INTO assets (symbol) VALUES ('AAPL');
+INSERT INTO assets (symbol) VALUES ('AMZN');
+INSERT INTO assets (symbol) VALUES ('FB');
+INSERT INTO assets (symbol) VALUES ('GOOG');
+INSERT INTO assets (symbol) VALUES ('MSTF');
+INSERT INTO assets (symbol) VALUES ('NFLX');
+INSERT INTO assets (symbol) VALUES ('TSLA');
+
+INSERT INTO quotes (bid, ask, last_price, volume, asset)
+VALUES (random(), random(), random(), random(), 'AAPL');
+
+INSERT INTO quotes (bid, ask, last_price, volume, asset)
+VALUES (random(), random(), random(), random(), 'AMZN');
+
+INSERT INTO quotes (bid, ask, last_price, volume, asset)
+VALUES (random(), random(), random(), random(), 'FB');
+
+INSERT INTO quotes (bid, ask, last_price, volume, asset)
+VALUES (random(), random(), random(), random(), 'GOOG');
+
+INSERT INTO quotes (bid, ask, last_price, volume, asset)
+VALUES (random(), random(), random(), random(), 'MSTF');
+
+INSERT INTO quotes (bid, ask, last_price, volume, asset)
+VALUES (random(), random(), random(), random(), 'NFLX');
+
+INSERT INTO quotes (bid, ask, last_price, volume, asset)
+VALUES (random(), random(), random(), random(), 'TSLA');
 
 COMMIT;
