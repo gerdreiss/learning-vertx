d1b9433d65117b980867cd0a029cfa18c15b60b8
Gerd Reiss <gerd@reiss.pro>
2021-11-03 21:48:40 +0100

    refactoring

--- udemy/learn-vertx/vertx-stock-broker/src/main/kotlin/stockbroker/verticles.kt
+++ udemy/learn-vertx/vertx-stock-broker/src/main/kotlin/stockbroker/verticles.kt
@@ -1,5 +1,6 @@
 package stockbroker
 
+import io.vertx.config.ConfigRetriever
 import io.vertx.core.AbstractVerticle
 import io.vertx.core.Promise
 import io.vertx.kotlin.core.deploymentOptionsOf
@@ -45,4 +46,38 @@ class MainVerticle : AbstractVerticle() {
   private fun processors(): Int =
     // to ensure that there is no problem in virtualized containers
     max(1, Runtime.getRuntime().availableProcessors())
+}
+
+class RestApiVerticle : AbstractVerticle() {
+
+  companion object {
+    private val logger: Logger = LoggerFactory.getLogger(RestApiVerticle::class.java)
+  }
+
+  override fun start(startPromise: Promise<Void>) {
+    ConfigRetriever.create(vertx)
+      .getConfig { result ->
+        if (result.failed()) {
+          startPromise.fail(result.cause())
+        } else {
+
+          val brokerConfig = BrokerConfig.fromJson(result.result())
+          val services = Services.create(vertx, brokerConfig)
+          val routes = Routes.create(vertx, services)
+
+          vertx
+            .createHttpServer()
+            .requestHandler(routes)
+            .exceptionHandler { logger.error("HTTP server error: $it") }
+            .listen(brokerConfig.socketAddress()) { httpServer ->
+              if (httpServer.succeeded()) {
+                startPromise.complete()
+                logger.info("HTTP server started on port ${httpServer.result().actualPort()}")
+              } else {
+                startPromise.fail(httpServer.cause())
+              }
+            }
+        }
+      }
+  }
 }
